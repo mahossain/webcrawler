381709301255a0bc2927d3f513f4ac92c947b87d
Mohammad Hossain <afzalhoss@gmail.com>
2017-08-31 16:56:55 +1000

    added controller & repository test

--- src/test/java/com/au/controller/WebCrawlerControllerTest.java
+++ src/test/java/com/au/controller/WebCrawlerControllerTest.java
@@ -1,10 +1,48 @@
 package com.au.controller;
 
-import static org.junit.Assert.*;
+import com.au.dao.CrawlerInfoRepository;
+import com.au.model.CrawlerInfo;
+import com.au.service.CrawlerServiceImpl;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
-/**
- * Created by dipu on 31/8/17.
- */
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Matchers.anyString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(WebCrawlerController.class)
 public class WebCrawlerControllerTest {
 
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private CrawlerServiceImpl crawlerService;
+
+    @MockBean
+    private CrawlerInfoRepository crawlerInfoRepository;
+
+    @Test
+    public void givenURL_thenReturnWebcrawlerInfoJsonArray() throws Exception {
+        CrawlerInfo crawlerInfo = new CrawlerInfo();
+        crawlerInfo.setId(UUID.randomUUID().toString());
+        crawlerInfo.setTitle("Home Page");
+        crawlerInfo.setUrl("http://google.com");
+        given(crawlerService.isValidURL(anyString())).willReturn(true);
+        given(crawlerInfoRepository.findByUrl(anyString())).willReturn(Optional.of(crawlerInfo));
+        mvc.perform(get("/crawler/rest/api/2?url=http://google.com")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
 }
--- src/test/java/com/au/dao/CrawlerInfoRepositoryTest.java
+++ src/test/java/com/au/dao/CrawlerInfoRepositoryTest.java
@@ -1,10 +1,36 @@
 package com.au.dao;
 
-import static org.junit.Assert.*;
+import com.au.model.CrawlerInfo;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-/**
- * Created by dipu on 31/8/17.
- */
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.junit.Assert.assertThat;
+
+@RunWith(SpringRunner.class)
+@DataMongoTest
 public class CrawlerInfoRepositoryTest {
 
+    @Autowired
+    private CrawlerInfoRepository crawlerInfoRepository;
+
+    @Test
+    public void whenFindByUrl_thenReturnCrawlerInfo() {
+        CrawlerInfo crawlerInfo = new CrawlerInfo();
+        crawlerInfo.setId(UUID.randomUUID().toString());
+        crawlerInfo.setTitle("Home Page");
+        crawlerInfo.setUrl("http://google.com");
+        crawlerInfoRepository.save(crawlerInfo);
+
+        Optional<CrawlerInfo> result = crawlerInfoRepository.findByUrl("http://google.com");
+        assertThat(result.isPresent(), Matchers.is(true));
+        assertThat(result.get().getTitle(), Matchers.is("Home Page"));
+        assertThat(result.get().getUrl(), Matchers.is("http://google.com"));
+    }
 }
